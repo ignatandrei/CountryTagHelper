157b92f2682f76f686a1045bd7fc9ae12f9191a1
ignatandrei <ignatandrei@yahoo.com>
2017-07-03 00:00:47 +0300

    first version

---
+++ CTHWeb/Controllers/CountryTagHelper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Threading.Tasks;
+
+namespace CTHWeb.Controllers
+{
+    public class CountryTagHelper:TagHelper
+    {
+        static string[] CountryISO;
+        static PropertyInfo[] properties;
+        static CountryTagHelper()
+        {
+            var t =typeof( ResCTH);
+            properties= t.GetProperties(
+                BindingFlags.Public |
+                BindingFlags.Static |
+                BindingFlags.GetProperty);
+            CountryISO = properties
+                .Where(it=>it.Name.Length==2)
+                .Select(it => it.Name)
+                .ToArray();
+        }
+        public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            
+            return base.ProcessAsync(context, output);
+        }
+    }
+}
---
+++ CTHWeb/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using CTHWeb.Controllers;
+
+namespace CTHWeb
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CountryTagHelper c = new CountryTagHelper();
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
