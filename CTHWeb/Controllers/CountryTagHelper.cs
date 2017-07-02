71d3295e7dee5f83b7f8dc7976efc39208ce0955
ignatandrei <ignatandrei@yahoo.com>
2017-07-03 00:37:41 +0300

    version 2 - adding functionality

--- CTHWeb/Controllers/CountryTagHelper.cs
+++ CTHWeb/Controllers/CountryTagHelper.cs
@@ -1,22 +1,35 @@
 ﻿using Microsoft.AspNetCore.Razor.TagHelpers;
 using Microsoft.Extensions.Localization;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Resources;
 using System.Threading.Tasks;
 
 namespace CTHWeb.Controllers
 {
+    /// <summary>
+    /// TODO: from IP
+    /// TODO: localized
+    /// TODO: first item empty
+    /// </summary>
+    [HtmlTargetElement("select", Attributes = ASPCountryAttributeName)]
     public class CountryTagHelper:TagHelper
     {
+        private const string ASPCountryAttributeName = "asp-country";
+        private const string ASPCountrySelectedAttributeName = "asp-country-selected";
+
+        [HtmlAttributeName(ASPCountryAttributeName)]
+        public bool ASPCountry { get; set; }
+
+        [HtmlAttributeName(ASPCountrySelectedAttributeName)]
+        public string ASPCountrySelected{ get; set; }
+
         static string[] CountryISO;
-        static PropertyInfo[] properties;
+        //static PropertyInfo[] properties;
         static CountryTagHelper()
         {
             var t =typeof( ResCTH);
-            properties= t.GetProperties(
+            var properties= t.GetProperties(
                 BindingFlags.Public |
                 BindingFlags.Static |
                 BindingFlags.GetProperty);
@@ -27,7 +40,24 @@ namespace CTHWeb.Controllers
         }
         public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
-            
+            if (ASPCountry)
+            {
+                bool existSelected = !string.IsNullOrWhiteSpace(ASPCountrySelected);
+                string option = "<option value='{0}' {2}>{1}</option>";
+                foreach (var item in CountryISO)
+                {
+                    string selected = "";
+                    string localizedName = ResCTH.ResourceManager.GetString(item);
+                    if (existSelected)
+                    {
+                        bool currentItem = string.Equals(item, ASPCountrySelected, StringComparison.CurrentCultureIgnoreCase);
+                        currentItem = currentItem || string.Equals(localizedName, ASPCountrySelected, StringComparison.CurrentCultureIgnoreCase);
+                        if (currentItem)
+                            selected = "selected";
+                    }
+                    output.Content.AppendFormat(option, item, localizedName,selected);
+                }
+            }
             return base.ProcessAsync(context, output);
         }
     }
